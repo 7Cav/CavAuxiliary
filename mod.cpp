3ff8cc11e6f78c3da27d94a73a5741a4368dde97
mazinskihenry <33608576+mazinskihenry@users.noreply.github.com>
2022-06-01 22:36:48 -0700

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,13 @@
+name = "7th Cavalry Auxiliary Mod";
+dir = "@Cav_Aux";
+picture = "";
+actionName = "";
+action = "";
+description = "aux";
+logo = "";
+logoOver = "";
+tooltip = "aux";
+tooltipOwned = "aux";
+overview = "";
+author = "Maz";
+overviewPicture = "";
